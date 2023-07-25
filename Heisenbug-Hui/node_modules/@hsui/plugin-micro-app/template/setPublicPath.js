b5c6ab12865003ae8d5bb710640e415221e2dde9
Chenn <2151414@tongji.edu.cn>
2023-07-25 18:02:20 +0800

    删除无用项目

--- Heisenbug-Hui/node_modules/@hsui/plugin-micro-app/template/setPublicPath.js
+++
@@ -1,3 +0,0 @@
-if (window.__POWERED_BY_HUI_MICRO_APP__) {
-  __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_HUI_MICRO_APP__;
-}
--- Heisenbug-Hui/src/.hui/huiExports.js
+++
@@ -1,3 +0,0 @@
-export * from '@tmp/micro-app/frameEmitter.js'
-export { default as singleSpaVue } from '@tmp/micro-app/singleSpaVue.js'
-export { default as store } from '@tmp/store.js'
